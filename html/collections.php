9279900d0423a9b70ebafa5358b5b4ac6c4cedc1
Rhev Gutierrez <rhev@nmgresources.ph>
2012-11-28 14:37:43 +0800

    adding product single.php

--- html/collections.php
+++ html/collections.php
@@ -23,51 +23,51 @@
                     </div>
                     <div class="collection-wrapper">
                     	<div class="prod-box">
-                        <p><a href="#">Collection Name 1</a></p>
+                        <p><a href="single.php">Collection Name 1</a></p>
                         	<img src="images/thumbnail1.png" />   
                         </div>
                         <div class="prod-box">
-                        <p><a href="#">Collection Name 2</a></p>
+                        <p><a href="single.php">Collection Name 2</a></p>
                         	<img src="images/thumbnail2.png" />
                         </div>
                         <div class="prod-box">
-                        <p><a href="#">Collection Name 3</a></p>
+                        <p><a href="single.php">Collection Name 3</a></p>
                         	<img src="images/thumbnail3.png" />
                         </div>
                         <div class="prod-box">
-                        <p><a href="#">Collection Name 4</a></p>
+                        <p><a href="single.php">Collection Name 4</a></p>
                         	<img src="images/thumbnail4.png" />
                         </div>
                         <div class="prod-box">
-                        <p><a href="#">Collection Name 5</a></p>
+                        <p><a href="single.php">Collection Name 5</a></p>
                         	<img src="images/thumbnail1.png" />
                         </div>
                         <div class="prod-box">
-                        <p><a href="#">Collection Name 6</a></p>
+                        <p><a href="single.php">Collection Name 6</a></p>
                         	<img src="images/thumbnail2.png" />
                         </div>
                         <div class="prod-box">
-                        <p><a href="#">Collection Name 7</a></p>
+                        <p><a href="single.php">Collection Name 7</a></p>
                         	<img src="images/thumbnail3.png" />
                         </div>
                         <div class="prod-box">
-                        <p><a href="#">Collection Name 8</a></p>
+                        <p><a href="single.php">Collection Name 8</a></p>
                         	<img src="images/thumbnail4.png" />
                         </div>
                         <div class="prod-box">
-                        <p><a href="#">Collection Name 9</a></p>
+                        <p><a href="single.php">Collection Name 9</a></p>
                         	<img src="images/thumbnail1.png" />
                         </div>
                         <div class="prod-box">
-                        <p><a href="#">Collection Name 10</a></p>
+                        <p><a href="single.php">Collection Name 10</a></p>
                         	<img src="images/thumbnail2.png" />
                         </div>
                         <div class="prod-box">
-                        <p><a href="#">Collection Name 11</a></p>
+                        <p><a href="single.php">Collection Name 11</a></p>
                         	<img src="images/thumbnail3.png" />
                         </div>
                         <div class="prod-box">
-                        <p><a href="#">Collection Name 12</a></p>
+                        <p><a href="single.php">Collection Name 12</a></p>
                         	<img src="images/thumbnail4.png" />
                         </div>
                         
---
+++ html/single.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Sfaera</title>
+<link rel="stylesheet" href="style.css" type="text/css" />
+</head>
+<body>
+<?php include("functions.php");?>
+<div id="main-wrapper">
+    <div id="main-container"> 
+    	<div id="container">   
+        	<div class="content-wrapper">        
+                <div id="global-container-left">
+                      <?php header_products(); ?>
+                      <?php _footer(); ?>
+                </div>
+                <div id="global-container-right">
+                <div class="singlehead-container">
+                    	<div class="single-title">
+                        	Collections
+                        </div>
+                        <div class="back-button-container">
+                        	<a href="collections.php">Back</a>
+                        </div>
+                    </div>
+                 <div class="single-product-img">
+                    	<img src="images/prod-landing.png" />
+                 </div>
+                 <div class="single-product-content">
+                 	<p><b>Item Name 3</b></p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sodales, sem et pretium semper, augue ligula auctor felis, eget bibendum velit mauris nec ipsum.</p>
+
+<p>Cras risus nulla, porttitor ut elementum non, fringilla quis mi. Morbi vitae lorem ante, ac rhoncus sapien. Morbi id sapien elit, sed ornare mauris. Mauris metus purus, pharetra ac condimentum ut, varius id justo. In hac habitasse platea dictumst. Duis eleifend tempus sodales. Aenean fringilla tincidunt eros a posuere.</p>
+                 </div>
+                </div>
+          </div>
+      </div>
+  </div>
+</div>
+</body>
+</html>
